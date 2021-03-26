f8460b469c813fcb3c76ec666a33f1c4e06a10b6
Andrew Murray <happyplace@users.noreply.github.com>
2021-03-25 23:47:41 -0400

    Queue is no longer exported

--- include/kiwi/KIWI_Queue.h
+++ include/kiwi/KIWI_Queue.h
@@ -12,24 +12,24 @@ extern "C" {
 
 	// creates a queue with a max capacity of maxCapacity and a element size of elementSize. returns NULL on error.
 	// The pointer returned by this function needs a call to KIWI_FreeQueue to free the memory
-	extern DECLSPEC struct KIWI_Queue* KIWI_CreateQueue(int elementSize, int maxCapacity);
+	struct KIWI_Queue* KIWI_CreateQueue(int elementSize, int maxCapacity);
 
 	// This frees all the memory associated with a queue created by KIWI_InitializeQueue
-	extern DECLSPEC void KIWI_FreeQueue(struct KIWI_Queue* queue);
+	void KIWI_FreeQueue(struct KIWI_Queue* queue);
 
 	// returns true if the queue is empty
-	extern DECLSPEC bool KIWI_QueueIsEmpty(struct KIWI_Queue* queue);
+	bool KIWI_QueueIsEmpty(struct KIWI_Queue* queue);
 
 	// returns true if the queue is full
-	extern DECLSPEC bool KIWI_QueueIsFull(struct KIWI_Queue* queue);
+	bool KIWI_QueueIsFull(struct KIWI_Queue* queue);
 
 	// pushes value into the end of queue
-	extern DECLSPEC void KIWI_QueuePush(struct KIWI_Queue* queue, void* value);
+	void KIWI_QueuePush(struct KIWI_Queue* queue, void* value);
 
 	// pops the first value in the queue and copies it into outValue.
 	// NOTE: outValue is assumed to be at least elementSize
 	// returns true if a value is found or false if the queue is empty
-	extern DECLSPEC bool KIWI_QueuePop(struct KIWI_Queue* queue, void* outValue);
+	bool KIWI_QueuePop(struct KIWI_Queue* queue, void* outValue);
 
 #ifdef __cplusplus
 }
