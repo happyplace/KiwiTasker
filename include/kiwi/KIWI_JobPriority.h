2ccb8e580d358eca20f4bdaec1194fc9bbb4f806
Andrew Murray <happyplace@users.noreply.github.com>
2021-03-26 21:34:46 -0400

    Job Priority enum

---
+++ include/kiwi/KIWI_JobPriority.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum KIWI_JobPriority
+{
+	KIWI_JobPriority_Low,
+	KIWI_JobPriority_Normal,
+	KIWI_JobPriority_High,
+};
+
+#ifdef __cplusplus
+}
+#endif
